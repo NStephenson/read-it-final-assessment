aa25369df06716f3571941afb37a47db5b827c64
NStephenson <nickstephenson90@gmail.com>
2016-06-17 20:11:43 -0400

    added sign in form

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,1 +1,9 @@
-var app = angular.module('readItApp', ['ui.router'])
+var app = angular.module('readItApp', ['ui.router', 'templates'])
+app.config(function($stateProvider){
+  $stateProvider
+  .state('signUp', {
+    url: '/signup',
+    controller: 'UserController',
+    templateUrl: 'app/templates/user/signup.html',
+  })
+})
